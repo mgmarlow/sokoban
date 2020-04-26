a1d547ffe7bf9aac0f836d2ec9606a3e75587402
Graham Marlow <mgmarlow@gmail.com>
2020-04-26 09:58:35 -0700

    Add reset button

--- src/states/PlayState.lua
+++ src/states/PlayState.lua
@@ -1,21 +1,15 @@
 PlayState = Class{__includes = BaseState}
 
 function PlayState:enter(params)
-  self.level = Level{source = params.tilemap}
-  self.player = Player {
-    x = self.level.playerX,
-    y = self.level.playerY
-  }
-
-  self.player.stateMachine = StateMachine {
-    ['walk'] = function() return PlayerWalkState(self.player, self.level) end,
-    ['idle'] = function() return PlayerIdleState(self.player) end,
-    ['push'] = function() return PlayerPushState(self.player, self.level) end,
-  }
-  self.player:changeState('idle')
+  self.tilemap = params.tilemap
+  self:initializeLevel()
 end
 
 function PlayState:update(dt)
+  if love.keyboard.wasPressed('r') then
+    self:reset()
+  end
+
   if self.level.victoriesNeeded > 0 and
      self.level.victoriesNeeded == self.level.victories then
        gStateMachine:change('victory')
@@ -28,4 +22,23 @@ end
 function PlayState:render()
   self.level:render()
   self.player:render()
+end
+
+function PlayState:reset()
+  self:initializeLevel()
+end
+
+function PlayState:initializeLevel()
+  self.level = Level{source = self.tilemap}
+  self.player = Player {
+    x = self.level.playerX,
+    y = self.level.playerY
+  }
+
+  self.player.stateMachine = StateMachine {
+    ['walk'] = function() return PlayerWalkState(self.player, self.level) end,
+    ['idle'] = function() return PlayerIdleState(self.player) end,
+    ['push'] = function() return PlayerPushState(self.player, self.level) end,
+  }
+  self.player:changeState('idle')
 end

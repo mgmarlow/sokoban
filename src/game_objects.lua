91499ecac5b14d47d96347b4aa97890658ac6b13
Graham Marlow <mgmarlow@gmail.com>
2020-04-26 11:02:03 -0700

    Add comment on tile index

--- src/game_objects.lua
+++ src/game_objects.lua
@@ -1,3 +1,14 @@
+--[[
+  Note on tile tileIndex:
+
+  Since lua indexes by 1 instead of 0, the Tiled
+  tile index equivalent is tileIndex - 1.
+
+  After loading the sokoban_tilesheet, click on an
+  individual tile to get its index. Add 1 to this
+  value and you'll get the exported *.lua equivalent.
+--]]
+
 GAME_OBJECT_DEFS = {
   ['wall'] = {
     tileIndex = 20,

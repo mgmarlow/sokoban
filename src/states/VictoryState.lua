40be465469b2b1984a5c8fab33ad5a3208ee83f1
Graham Marlow <mgmarlow@gmail.com>
2020-05-07 19:11:49 -0700

    Add option to go to next level from victory state

--- src/states/VictoryState.lua
+++ src/states/VictoryState.lua
@@ -1,17 +1,20 @@
 VictoryState = Class {__includes = BaseState}
 
 function VictoryState:enter(params)
-  self.levelNumber = params.levelNumber
+  self.next = params.levelNumber + 1
+  self.showNext = self.next <= gLoader.numLevels
 end
 
 function VictoryState:update(dt)
-  local next = self.levelNumber + 1
+  if love.keyboard.wasPressed('z') and self.showNext then
+    gStateMachine:change('play', {levelNumber = self.next})
+  end
 
-  if love.keyboard.wasPressed('z') then
+  if love.keyboard.wasPressed('x') then
     gStateMachine:change(
       'level_select',
       {
-        selectedLevel = next
+        selectedLevel = self.next
       }
     )
   end
@@ -35,10 +38,21 @@ function VictoryState:render()
   -- instructions
   love.graphics.setFont(gFonts['medium'])
   love.graphics.setColor(0, 255, 0, 255)
+
+  if self.showNext then
+    love.graphics.printf(
+      'Press Z to go to next level',
+      0,
+      WINDOW_HEIGHT / 2 + 70,
+      WINDOW_WIDTH,
+      'center'
+    )
+  end
+
   love.graphics.printf(
-    'Press Z to go to next level',
+    'Press X to go to level select',
     0,
-    WINDOW_HEIGHT / 2 + 70,
+    WINDOW_HEIGHT / 2 + 120,
     WINDOW_WIDTH,
     'center'
   )
